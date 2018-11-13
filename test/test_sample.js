e4abbd55f9595e8e32f522d4a0cf254b3b10ff6f
FND <fnd@localhost.localdomain>
2018-11-13 17:16:23 +0100

    added Mocha sample

---
+++ test/test_sample.js
@@ -0,0 +1,10 @@
+/* global describe, it */
+"use strict";
+
+let assert = require("assert");
+
+describe("sample", () => {
+	it("should illustrate basics", () => {
+		assert.strictEqual(1, 1, "oops");
+	});
+});
